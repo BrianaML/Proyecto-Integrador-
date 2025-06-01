c215a4125b56fd791ae1aa51d3d8e46d72df723d
7Delka <santiagomk42@gmail.com>
2025-06-01 01:30:02 -0300

    subiendo programa

--- js/cate2.js
+++ js/cate2.js
@@ -1,43 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let busqueda= location.search
-    let queque= new URLSearchParams(busqueda);
-    let category= queque.get("category");
-    
-    console.log(category);
-    
-    cargarProductos(category);
-    let linkCat = document.querySelector("#linkCat");
-    linkCat.innerText = category;
-    document.querySelectorAll("a.a-cat").forEach(function(categoryAnchor) {
-        categoryAnchor.setAttribute("href", `category.html?category=${category}`);
-    });
+let se = document.querySelector(".productos")
+let boton = document.querySelector(".desc")
+let comprar = document.querySelector(".agregara")
 
-        function cargarProductos(category){
-            fetch(`https://fakestoreapi.com/products/category/${category}`)
-                .then(function(res){
-                    return res.json();
-                })
-                .then(function(data){
-                    console.log(data)
-                    let prodList = document.querySelector('#container-products-cate');
-                    prodList.textContent = '';
-                    data.forEach(function(item){
-                    let template= document.createElement('div');
-                        template.className = "col-4";
-                    let truncaDes= item.description.length > 100 ? item.description.substring(0, 100) + '...' : item.description;
-                        template.innerHTML = `
-                            <img src="${item.image}" alt="">
-                            <h3 class="cat-title"><a href="producto.html?id=${item.id}">${item.title}</a></h3>
-                            <p class="index-des">Descripción:</p>
-                            <p>${truncaDes}</p>
-                            <p>$${item.price.toFixed(2)}USD</p>
-                            <h4><a href="producto.html?id=${item.id}" class="ver-1">Ver mas</a></h4>
-                        `;
-                    prodList.append(template);
-                });
-        }).catch(function (error) {
-                    console.log(error);
-                })
-            }
-        });
-    //cargarProductos(category);
+let datosEnLocal = localStorage.getItem("datosLocalStorage");
+
+let datosparseados = JSON.parse(datosEnLocal)
+if (datosEnLocal == null){
+    se.innerHTML = "<h2>Su Carriton esta vacio</h2>"
+    boton.style.display = "none";
+} else {
+    for(i=0;i<datosparseados.length;i++){
+        fetch (`https://fakestoreapi.com/products/${datosparseados[i].id}`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            se.innerHTML += `
+            <section class="prodindividual">
+            <img class="section-images" src="${data.image}">
+            <article class="text">
+            <h3>${data.title}</h3>
+            <p>$${data.price}</p>
+            </article>
+            </section`
+        })
+    .catch (function(err){
+        console.log(err)
+    })
+    comprar.addEventListener("click", function(){
+        localStorage.removeItem("datosLocalStorage");
+        alert("GRACIAS POR SU COMPRA");
+    })
+    }
+}
